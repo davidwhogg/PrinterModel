0a6484f71d7beec25101f66cb2d5168baa789d60
David W. Hogg <dwhogg@gmail.com>
2012-02-05 15:25:03 -0500

    tiny tweaks

--- py/printer.py
+++ py/printer.py
@@ -1,18 +1,20 @@
-# This file is part of PrinterModel.
-# Copyright 2012 David W. Hogg (NYU).
+'''
+This file is part of PrinterModel.
+Copyright 2012 David W. Hogg (NYU) <http://cosmo.nyu.edu/hogg/>.
 
-# PrinterModel is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License, version 2, as
-# published by the Free Software Foundation.
+PrinterModel is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
 
-# PrinterModel is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
+PrinterModel is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
 
-# You should have received a copy of the GNU General Public License
-# version 2 along with this program.  If not, see
-# <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+You should have received a copy of the GNU General Public License
+version 2 along with this program.  If not, see
+<http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+'''
 
 # to-do
 # -----
